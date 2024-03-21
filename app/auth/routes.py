712995b3f039eb10f60912e3914e584fa080906c
DerFacn <nikitalupalo145@gmail.com>
2024-03-21 17:48:55 +0100

    Updated auth routes + utils

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -7,57 +7,57 @@ from app.utils import get_user
 
 
 @get_user
-def signup(user):
-    if user:
-        return redirect(url_for('main.index'))
+def signup(user_):
+    if user_:
+        return redirect(url_for('main.index'))  # If already logged in - redirect
 
     title = 'Signup'
 
-    if request.method == 'GET':
-        return render_template('auth/signup.html', title=title)
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = session.query(User).filter_by(username=username).first()
 
-    username = request.form.get('username')
-    password = request.form.get('password')
-    user = session.query(User).filter_by(username=username).first()
+        if user:
+            flash('Username already in use.')
+            return render_template('auth/signup.html', title=title)
 
-    if user:
-        flash('Username already in use.')
-        return render_template('auth/signup.html', title=title)
+        user = User(username=username, password=generate_hash(password))
+        session.add(user)
+        session.commit()
+        s['user_id'] = user.id
 
-    user = User(username=username, password=generate_hash(password))
-    session.add(user)
-    session.commit()
-    s['user'] = user.id
+        return redirect(url_for('main.index'))
 
-    return redirect(url_for('main.index'))
+    return render_template('auth/signup.html', title=title)  # Opened in browser
 
 
 @get_user
-def login(user):
-    if user:
+def login(user_):
+    if user_:
         return redirect(url_for('main.index'))
 
     title = 'Login'
 
-    if request.method == 'GET':
-        return render_template('auth/signup.html', title=title)
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = session.query(User).filter_by(username=username).first()
 
-    username = request.form.get('username')
-    password = request.form.get('password')
-    user = session.query(User).filter_by(username=username).first()
+        if not user:
+            flash('User not founded.')
+            return render_template('auth/signup.html', title=title)
 
-    if not user:
-        flash('User not founded.')
-        return render_template('auth/signup.html', title=title)
+        if not check_password(password, user.password):
+            flash('Wrong password.')
+            return render_template('auth/signup.html', title=title)
 
-    if not check_password(password, user.password):
-        flash('Wrong password.')
-        return render_template('auth/signup.html', title=title)
+        s['user_id'] = user.id
+        return redirect(url_for('main.index'))
 
-    s['user'] = user.id
-    return redirect(url_for('main.index'))
+    return render_template('auth/signup.html', title=title)
 
 
 def logout():
-    s['user'] = None
+    s['user_id'] = None
     return redirect(url_for('main.index'))
--- app/utils.py
+++ app/utils.py
@@ -7,11 +7,11 @@ from functools import wraps
 def get_user(func):
     @wraps(func)
     def wrapped(*args, **kwargs):
-        kwargs['user'] = None
-        user_id = s.get('user')
+        kwargs['user_'] = None
+        user_id = s.get('user_id')
         if user_id:
             user = session.query(User).filter_by(id=user_id).first()
-            kwargs['user'] = user
+            kwargs['user_'] = user
             return func(*args, **kwargs)
         return func(*args, **kwargs)
     return wrapped
