ff2a093b023ab5e9aef2d1ea8388c89ca9c4169d
DerFacn <nikitalupalo145@gmail.com>
2024-05-11 19:54:06 +0200

    Signup and login forms updated

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -5,4 +5,4 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 bp.add_url_rule('/signup', 'signup', signup, methods=['GET', 'POST'])
 bp.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
-bp.add_url_rule('/logout', 'logout', logout, methods=['POST'])
+bp.add_url_rule('/logout', 'logout', logout, methods=['GET', 'POST'])
